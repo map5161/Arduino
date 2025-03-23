13b0fb8366cf41cbd7e4929c3756befa71930dc1
Mark Polidor <map5161@gmail.com>
2025-03-23 16:09:30 -0400

    Adding IK and Joystick code

---
+++ MeArm_Movement_Code_01_Inverse_Kinematics/MeArm_Movement_Code_01_Inverse_Kinematics.ino
@@ -0,0 +1,53 @@
+/* MeArm IK test - York Hackspace May 2014
+ * Test applying Nick Moriarty's Inverse Kinematics solver
+ * to MeArm Robotics Classic MeArm robot arm, to walk a specified path.
+ *
+ * Pins:
+ * Arduino    Base   Shoulder  Elbow    Claw
+ *    GND    Brown     Brown   Brown     Brown
+ *     5V      Red       Red     Red       Red
+ *     11    Yellow
+ *     10             Yellow
+ *      9                     Yellow
+ *      6                               Yellow
+ */
+#include "MeArm.h"
+#include <Servo.h>
+
+int basePin = 11;
+int shoulderPin = 10;
+int elbowPin = 9;
+int clawPin = 6;
+
+/*
+//ESP8266 MeArm Wifi PCB
+int basePin = 16;
+int shoulderPin = 14;
+int elbowPin = 12;
+int clawPin = 13;
+*/
+
+MeArm arm;
+
+void setup() {
+  arm.begin(basePin, shoulderPin, elbowPin, clawPin);
+}
+
+void loop() {
+  //Clap - so it's obvious we're at this part of the routine
+  arm.openClaw();
+  arm.closeClaw();
+  arm.openClaw();
+  arm.closeClaw();
+  arm.openClaw();
+  delay(500);
+  //Go up and left to grab something
+  arm.moveToXYZ(-80,100,140); 
+  arm.closeClaw();
+  //Go down, forward and right to drop it
+  arm.moveToXYZ(70,200,10);
+  arm.openClaw();
+  //Back to start position
+  arm.moveToXYZ(0,100,50);
+  delay(2000);
+}
---
+++ MeArm_Movement_Code_02_Joystick/MeArm_Movement_Code_02_Joystick.ino
@@ -0,0 +1,59 @@
+
+/* MeArm IK joysticks - York Hackspace May 2014
+ * Using inverse kinematics with joysticks
+ * Uses two analogue joystcks (two pots each)
+ * First stick moves claw forwards, backwards, left and right
+ * Second stick moves claw up, down, and closes and opens.
+ * 
+ * I used Sparkfun thumbstick breakout boards, oriented 'upside down'.
+ * 
+ * Pins:
+ * Arduino    Stick1    Stick2    Base   Shoulder  Elbow    Claw
+ *    GND       GND       GND    Brown     Brown   Brown     Brown
+ *     5V       VCC       VCC      Red       Red     Red       Red
+ *     A0       HOR
+ *     A1       VER
+ *     A2                 HOR
+ *     A3                 VER
+ *     11                       Yellow
+ *     10                                 Yellow
+ *      9                                         Yellow
+ *      6                                                   Yellow
+ */
+#include "MeArm.h"
+#include <Servo.h>
+
+int basePin = 11;
+int shoulderPin = 10;
+int elbowPin = 9;
+int clawPin = 6;
+
+int xdirPin = 0;
+int ydirPin = 1;
+int zdirPin = 3;
+int gdirPin = 2;
+
+MeArm arm;
+
+void setup() {
+  arm.begin(basePin, shoulderPin, elbowPin, clawPin);
+}
+
+void loop() {
+  float dx = map(analogRead(xdirPin), 0, 1023, -5.0, 5.0);
+  float dy = map(analogRead(ydirPin), 0, 1023, 5.0, -5.0);
+  float dz = map(analogRead(zdirPin), 0, 1023, 5.0, -5.0);
+  float dg = map(analogRead(gdirPin), 0, 1023, 5.0, -5.0);
+  if (abs(dx) < 1.5) dx = 0;
+  if (abs(dy) < 1.5) dy = 0;
+  if (abs(dz) < 1.5) dz = 0;
+  
+  if (!(dx == 0 && dy == 0 && dz == 0))
+    arm.snapToXYZ(arm.getX() + dx, arm.getY() + dy, arm.getZ() + dz);
+  
+  if (dg < -3.0)
+    arm.closeClaw();
+  else if (dg > 3.0)
+    arm.openClaw();  
+  delay(50);
+}
